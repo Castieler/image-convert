619c579238e96cf92b9e1d997e6549c55e72469e
xiamingyu <xiamingyu@powerlaw.ai>
2023-03-09 23:09:39 +0800

    update

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # 镜像转换工具
 ## 使用 
-- 推送镜像
-- 拉取镜像
+### 推送镜像
+
+- 
+    `./image-convert --docker-hub-user {docker hub user} --push --s-image quay.io/jetstack/cert-manager-cainjector:v1.10.0`
+### 拉取镜像
+
+    `./image-convert --docker-hub-user {docker hub user} --pull --s-image quay.io/jetstack/cert-manager-cainjector:v1.10.0`
--- main.go
+++ main.go
@@ -1,82 +1,111 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
 	"os"
 	"os/exec"
 	"strings"
-
-	"github.com/spf13/cobra"
 )
 
-var (
+type ImageConvert struct {
 	push             bool
 	pull             bool
 	tagDockerHubUser string
 	sourceImage      string
-)
-
-var rootCmd = &cobra.Command{
-	Use:   "hugo",
-	Short: "Hugo is a very fast static site generator",
-	Long: `A Fast and Flexible Static Site Generator built with
-                love by spf13 and friends in Go.
-                Complete documentation is available at https://gohugo.io/documentation/`,
-	Run: func(cmd *cobra.Command, args []string) {
-		list1 := strings.Split(sourceImage, ":")
-		version := list1[1]
-		list2 := strings.Split(list1[0], "/")
-		imageName := list2[len(list2)-1]
-		trgImageName := fmt.Sprintf("%s/%s:%s", tagDockerHubUser, imageName, version)
-		if push {
-			tagCmd := exec.Command("docker", "tag", sourceImage, trgImageName)
-			stdout, err := tagCmd.Output()
-			if err != nil {
-				fmt.Println(err.Error())
-				return
-			}
-			fmt.Println(string(stdout))
-			pushCmd := exec.Command("docker", "push", trgImageName)
-			stdout, err = pushCmd.Output()
-			if err != nil {
-				fmt.Println(err.Error())
-				return
-			}
-			fmt.Println(string(stdout))
-			return
-		}
-
-		if pull {
-			pushCmd := exec.Command("docker", "pull", trgImageName)
-			stdout, err := pushCmd.Output()
-			if err != nil {
-				fmt.Println(err.Error())
-				return
-			}
-			fmt.Println(string(stdout))
-
-			tagCmd := exec.Command("docker", "tag", trgImageName, sourceImage)
-			stdout, err = tagCmd.Output()
-			if err != nil {
-				fmt.Println(err.Error())
-				return
-			}
-			fmt.Println(string(stdout))
-			return
-		}
-	},
+	rootCmd          *cobra.Command
 }
 
-func Execute() {
-	if err := rootCmd.Execute(); err != nil {
+func New() ImageConvert {
+	ic := ImageConvert{}
+	var rootCmd = &cobra.Command{
+		Use:   "image-convert",
+		Short: "镜像转换",
+		Run: func(cmd *cobra.Command, args []string) {
+			err := Convert(ic.sourceImage, ic.tagDockerHubUser, ic.push, ic.pull)
+			if err != nil {
+				logrus.Error(err)
+				return
+			}
+		},
+	}
+	ic.rootCmd = rootCmd
+	rootCmd.PersistentFlags().StringVar(&ic.sourceImage, "s-image", "", "原始镜像名")
+	rootCmd.PersistentFlags().StringVar(&ic.tagDockerHubUser, "docker-hub-user", "1083298593", "原始镜像名")
+	rootCmd.PersistentFlags().BoolVar(&ic.push, "push", false, "推送镜像，默认为 true")
+	rootCmd.PersistentFlags().BoolVar(&ic.pull, "pull", false, "拉取镜像，默认为 false")
+	return ImageConvert{
+		rootCmd: rootCmd,
+	}
+}
+
+func (ic *ImageConvert) Execute() {
+	if err := ic.rootCmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
 func main() {
-	rootCmd.PersistentFlags().StringVar(&sourceImage, "s-image", "", "原始镜像名")
-	rootCmd.PersistentFlags().StringVar(&tagDockerHubUser, "docker-hub-user", "1083298593", "原始镜像名")
-	rootCmd.PersistentFlags().BoolVar(&push, "push", false, "推送镜像，默认为 true")
-	rootCmd.PersistentFlags().BoolVar(&pull, "pull", false, "拉取镜像，默认为 false")
-	rootCmd.Execute()
+	ic := New()
+	ic.Execute()
+}
+
+func Convert(sourceImage, tagDockerHubUser string, push, pull bool) error {
+	if len(sourceImage) == 0 {
+		return errors.New("需要指定 --s-image ")
+	}
+	if !push && !pull {
+		return errors.New("需要指定 --pull 或 --push ")
+	}
+	list1 := strings.Split(sourceImage, ":")
+	version := list1[1]
+	list2 := strings.Split(list1[0], "/")
+	imageName := list2[len(list2)-1]
+	trgImageName := fmt.Sprintf("%s/%s:%s", tagDockerHubUser, imageName, version)
+	if push {
+		logrus.Infof("拉取镜像(%s)中...", sourceImage)
+		pullCmd := exec.Command("docker", "pull", sourceImage)
+		stdout, err := pullCmd.Output()
+		if err != nil {
+			return err
+		}
+		logrus.Infof("镜像(%s -> %s)转换中...", sourceImage, trgImageName)
+		tagCmd := exec.Command("docker", "tag", sourceImage, trgImageName)
+		stdout, err = tagCmd.Output()
+		if err != nil {
+			return err
+		}
+		logrus.Info(string(stdout))
+
+		logrus.Infof("镜像(%s)推送中...", trgImageName)
+		pushCmd := exec.Command("docker", "push", trgImageName)
+		stdout, err = pushCmd.Output()
+		if err != nil {
+			return err
+		}
+		logrus.Info(string(stdout))
+		return nil
+	}
+
+	if pull {
+		logrus.Infof("拉取镜像(%s)中...", trgImageName)
+		pushCmd := exec.Command("docker", "pull", trgImageName)
+		stdout, err := pushCmd.Output()
+		if err != nil {
+			return err
+		}
+		logrus.Info(string(stdout))
+
+		logrus.Infof("镜像(%s -> %s)转换中...", trgImageName, sourceImage)
+		tagCmd := exec.Command("docker", "tag", trgImageName, sourceImage)
+		stdout, err = tagCmd.Output()
+		if err != nil {
+			return err
+		}
+		logrus.Info(string(stdout))
+		return err
+	}
+	return nil
 }
